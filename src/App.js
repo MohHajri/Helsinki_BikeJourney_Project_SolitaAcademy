545561f0d9ebb2e28eae4886d33eaaa80389d8f3
hajri <c1almo00@students.oamk.fi>
2023-05-12 10:56:28 +0300

    landig page added, more strcuture added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React, { useState } from 'react';
 import './styles/App.css';
+import BikeTrips from './components/BikeTrips.js';
+import SingleStation from './components/SingleStation.js';
 
 function App() {
+  const [showBikeTrips, setShowBikeTrips] = useState(true);
+
+  const handleToggleView = () => {
+    setShowBikeTrips(!showBikeTrips);
+  };
+
   return (
     <div className="App">
-     
+      <h1>Test page</h1>
+      <button onClick={handleToggleView}>Toggle View</button>
+      {showBikeTrips ? (
+        <BikeTrips />
+      ) : (
+        <SingleStation stationName="Example Station" />
+      )}
     </div>
   );
 }
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import '../styles/LandingPage.css';
+import bike_vector from '../assets/bike_vector.png';
+
+function LandingPage() {
+  return (
+    <div className="container">
+      <Navbar />
+      <div className="content">
+        <div className="content__item-container">
+          <div className="content__item">
+            <h1 className="content__title">
+              Explore Helsinki's Bike Adventures
+            </h1>
+            <p className="content__text">
+              Discover popular bike routes and stations in the city with our curated trips,
+              or create your own adventure using our feature.
+              Get ready to explore Helsinki's bike culture in a whole new way.
+            </p>
+            <button className="content__button">Create a Bike Trip</button>
+          </div>
+          <div className="content__image">
+            <img src={bike_vector} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
